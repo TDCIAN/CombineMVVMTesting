364d49f1601e0f62fa01bfc7ec457b3497456bf3
TDCIAN <tdcian71@gmail.com>
2023-04-27 08:52:43 +0900

    config base settings

---
+++ KelvinFokTipCalculator/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  KelvinFokTipCalculator
+//
+//  Created by JeongminKim on 2023/04/27.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+    }
+
+}
+
---
+++ KelvinFokTipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KelvinFokTipCalculator
+//
+//  Created by JeongminKim on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
