2099cd7545cb9b7a56a193e5cc0e0b79257f3875
TDCIAN <tdcian71@gmail.com>
2023-04-28 11:46:39 +0900

    config snapshot test

---
+++ KelvinFokTipCalculatorTests/KelvinFokTipCalculatorSnapshotTest.swift
@@ -0,0 +1,129 @@
+//
+//  tip_calculatorSnapshotTest.swift
+//  KelvinFokTipCalculatorTests
+//
+//  Created by JeongminKim on 2023/04/28.
+//
+
+import XCTest
+import SnapshotTesting
+
+@testable import KelvinFokTipCalculator
+
+final class KelvinFokTipCalculatorSnapshotTest: XCTestCase {
+    
+    private var screenWidth: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+    
+    func testLogoView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 48)
+        // when
+        let view = LogoView()
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testInitialResultView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 224)
+        // when
+        let view = ResultView()
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testResultViewWithValues() {
+        // given
+        let size = CGSize(width: screenWidth, height: 224)
+        let result = Result(
+            amountPerPerson: 100.25,
+            totalBill: 45,
+            totalTip: 60
+        )
+        // when
+        let view = ResultView()
+        view.configure(result: result)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testInitialBillInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        // when
+        let view = BillInputView()
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testBillInputViewWithValues() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        // when
+        let view = BillInputView()
+        let textField = view.allSubViewsOf(type: UITextField.self).first
+        textField?.text = "500"
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testTipInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 128)
+        // when
+        let view = TipInputView()
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testTipInputViewWithSelection() {
+        // given
+        let size = CGSize(width: screenWidth, height: 128)
+        // when
+        let view = TipInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).first
+        button?.sendActions(for: .touchUpInside)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testSplitInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        // when
+        let view = SplitInputView()
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+    
+    func testSplitInputViewWithSelection() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        // when
+        let view = SplitInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).last
+        button?.sendActions(for: .touchUpInside)
+        // then
+        assertSnapshot(matching: view, as: .image(size: size), record: false)
+    }
+}
+
+extension UIView {
+    // https://stackoverflow.com/a/45297466/6181721
+    func allSubViewsOf<T: UIView>(type: T.Type) -> [T] {
+        var all = [T]()
+        func getSubview(view: UIView) {
+            if let aView = view as? T {
+                all.append(aView)
+            }
+            guard view.subviews.count > 0 else { return }
+            view.subviews.forEach {
+                getSubview(view: $0)
+            }
+        }
+        getSubview(view: self)
+        return all
+    }
+}
